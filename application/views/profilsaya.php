3c5110b09e168f4ee5f8414d545dd212afa70792
raselstr <str.rasel29@gmail.com>
2023-02-15 00:05:11 +0700

    Update profilsaya.php

--- application/views/profilsaya.php
+++ application/views/profilsaya.php
@@ -1,6 +1,6 @@
 <h1>Halaman Profil</h1> 
 <hr>
-<p>Nama : Nasrun Marpaung</p>
+<p>Nama : RAHMAD SALEH SITORUS</p>
 <p>Kelas : Transfer</p>
 <p>Alamat : <?= $alamat ?></p>
 <p>Jenis Kelamin : <?= $jk ?></p>
